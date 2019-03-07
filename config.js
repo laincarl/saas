7304f2a2f462ce1e799b03edaae5554041ad0784
laincarl <1047972853@qq.com>
2019-03-07 16:46:54 +0800

    init

---
+++ config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    resolve: {    
+      alias: {
+        Loading: path.resolve(__dirname, './src/Loading.js'),
+      },
+    },
+    devServer: {
+      open: true,
+      port: 3030,
+    },
+  },
+  // envs: {
+  //   API: 'http://localhost',
+  // },  
+};

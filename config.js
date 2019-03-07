906daf4883ab0cd7ea1f1951914badbb7091329d
laincarl <1047972853@qq.com>
2019-03-07 17:04:42 +0800

    improve function

--- config.js
+++ config.js
@@ -4,7 +4,12 @@ module.exports = {
   webpack: {
     resolve: {    
       alias: {
-        Loading: path.resolve(__dirname, './src/Loading.js'),
+        Loading: path.resolve(__dirname, './src/components/loading'),
+        api: path.resolve(__dirname, './src/api'),
+        assets: path.resolve(__dirname, './src/assets'),
+        components: path.resolve(__dirname, './src/components'),
+        pages: path.resolve(__dirname, './src/pages'),
+        stores: path.resolve(__dirname, './src/stores'),
       },
     },
     devServer: {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createBrowserHistory } from 'history';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Home } from 'pages';
 
-const App = () => <div>React booter</div>;
+const stores = {
+
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Provider {...stores}>
+        <Router history={createBrowserHistory}>          
+          <Switch>
+            <Route exact path="/" component={Home} />            
+            {/* 其他重定向到 404 */}
+            <Redirect from="*" to="/404" />
+          </Switch>          
+        </Router>
+      </Provider>
+    );
+  }
+}
 export default App;
